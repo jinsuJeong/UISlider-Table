b1c5f0df3f8cc25e9b66d624acee2171d420f2c8
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-05-14 15:46:25 +0900

    UISliderTable!!

--- UISlider-Table/ViewController.swift
+++ UISlider-Table/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var tabel: UITableView!
+    var i=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tabel.delegate=self
+        tabel.dataSource=self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderMoved(_ sender: Any) {
+        print("\(mySlider.value)slider moved!")
+        myLabel.text=String(Int(mySlider.value))
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tabel.dequeueReusableCell(withIdentifier: "re", for: indexPath)
+        cell.textLabel?.text=String(Int(mySlider.value)+indexPath.row)
+        //i=Int(mySlider.value)
+        return cell
+    }
+    @IBAction func sli(_ sender: Any) {
+        tabel.reloadData()
+    }
 }
 
